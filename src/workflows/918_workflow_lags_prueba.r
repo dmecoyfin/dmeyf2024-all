d7fba3c4f5a1141d085604f1c26d33af5ee8d931
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-10-24 14:07:59 +0000

    auto commit 1       20241024 140751

--- src/workflows/918_workflow_lags_prueba.r
+++ src/workflows/918_workflow_lags_prueba.r
@@ -137,8 +137,8 @@ FEhist_base <- function( pinputexps)
   param_local$lag1 <- TRUE
   param_local$lag2 <- FALSE # no me engraso con los lags de orden 2
   param_local$lag3 <- FALSE # no me engraso con los lags de orden 3
-  param_local$lag6 <- FALSE # no me engraso con los lags de orden 6
-  param_local$lag12 <- FALSE # no me engraso con los lags de orden 12
+  param_local$lag6 <- FALSE # no me engraso con los lags de orden 3
+  param_local$lag12 <- FALSE # no me engraso con los lags de orden 3
 
   # no me engraso las manos con las tendencias
   param_local$Tendencias1$run <- TRUE  # FALSE, no corre nada de lo que sigue
