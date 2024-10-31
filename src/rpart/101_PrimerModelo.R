b5f3004bf4d89a31b9169d891d51133c3297c760
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-10-31 02:46:41 +0000

    auto commit 17       20241031 024628

--- src/rpart/101_PrimerModelo.R
+++ src/rpart/101_PrimerModelo.R
@@ -1,65 +1,46 @@
-# Arbol elemental con libreria  rpart
-# Debe tener instaladas las librerias  data.table ,  rpart  y  rpart.plot
+require( "data.table" )
 
-# cargo las librerias que necesito
-require("data.table")
-require("rpart")
-require("rpart.plot")
+# leo el dataset
+dataset <- fread("competencia_01_crudo.csv" )
 
-# Aqui se debe poner la carpeta de la materia de SU computadora local
-setwd("~/buckets/b1") # Establezco el Working Directory
-
-# cargo el dataset que tiene la clase calculada !
-dataset <- fread("./datasets/competencia_01.csv")
-
-dtrain <- dataset[foto_mes <= 202104] # defino donde voy a entrenar
-dapply <- dataset[foto_mes == 202106] # defino donde voy a aplicar el modelo
-
-# genero el modelo,  aqui se construye el arbol
-# quiero predecir clase_ternaria a partir de el resto de las variables
-modelo <- rpart(
-    formula = "clase_ternaria ~ .",
-    data = dtrain, # los datos donde voy a entrenar
-    xval = 0,
-    cp = -1, # esto significa no limitar la complejidad de los splits
-    minsplit = 250, # minima cantidad de registros para que se haga el split
-    minbucket = 100, # tamaño minimo de una hoja
-    maxdepth = 7  # profundidad maxima del arbol
-)
+# calculo el periodo0 consecutivo
+dsimple <- dataset[, list(
+  "pos" = .I,
+  numero_de_cliente,
+  periodo0 = as.integer(foto_mes/100)*12 +  foto_mes%%100 ) ]
 
 
-# grafico el arbol
-prp(modelo,
-    extra = 101, digits = -5,
-    branch = 1, type = 4, varlen = 0, faclen = 0
-)
+# ordeno
+setorder( dsimple, numero_de_cliente, periodo0 )
+
+# calculo topes
+periodo_ultimo <- dsimple[, max(periodo0) ]
+periodo_anteultimo <- periodo_ultimo - 1
 
 
-# aplico el modelo a los datos nuevos
-prediccion <- predict(
-    object = modelo,
-    newdata = dapply,
-    type = "prob"
-)
+# calculo los leads de orden 1 y 2
+dsimple[, c("periodo1", "periodo2") :=
+          shift(periodo0, n=1:2, fill=NA, type="lead"),  numero_de_cliente ]
 
-# prediccion es una matriz con TRES columnas,
-# llamadas "BAJA+1", "BAJA+2"  y "CONTINUA"
-# cada columna es el vector de probabilidades
+# assign most common class values = "CONTINUA"
+dsimple[ periodo0 < periodo_anteultimo, clase_ternaria := "CONTINUA" ]
 
-# agrego a dapply una columna nueva que es la probabilidad de BAJA+2
-dapply[, prob_baja2 := prediccion[, "BAJA+2"]]
+# calculo BAJA+1
+dsimple[ periodo0 < periodo_ultimo &
+           ( is.na(periodo1) | periodo0 + 1 < periodo1 ),
+         clase_ternaria := "BAJA+1" ]
 
-# solo le envio estimulo a los registros
-#  con probabilidad de BAJA+2 mayor  a  1/40
-dapply[, Predicted := as.numeric(prob_baja2 > 1 / 40)]
+# calculo BAJA+2
+dsimple[ periodo0 < periodo_anteultimo & (periodo0+1 == periodo1 )
+         & ( is.na(periodo2) | periodo0 + 2 < periodo2 ),
+         clase_ternaria := "BAJA+2" ]
 
-# genero el archivo para Kaggle
-# primero creo la carpeta donde va el experimento
-dir.create("./exp/")
-dir.create("./exp/KA2001")
 
-# solo los campos para Kaggle
-fwrite(dapply[, list(numero_de_cliente, Predicted)],
-        file = "./exp/KA2001/K101_001.csv",
+# pego el resultado en el dataset original y grabo
+setorder( dsimple, pos )
+dataset[, clase_ternaria := dsimple$clase_ternaria ]
+
+fwrite( dataset,
+        file =  "competencia_01_R.csv",
         sep = ","
 )
