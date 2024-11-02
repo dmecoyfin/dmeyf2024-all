dbf95b2422d6c5ac30eb0c6c7276ea335554f753
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-11-02 11:40:28 +0000

    auto commit 3       20241102 114020

--- src/workflows/918_workflow_base_f202108_canaritos.r
+++ src/workflows/918_workflow_base_f202108_canaritos.r
@@ -425,7 +425,7 @@ wf_agosto_base_canaritos <- function( pnombrewf )
   param_local <- exp_wf_init( pnombrewf ) # linea workflow inicial fija
 
   # Etapa especificacion dataset de la Segunda Competencia Kaggle
-  DT_incorporar_dataset( "~/buckets/b1/datasets/dataset_iter_k.csv.gz")
+  DT_incorporar_dataset( "~/buckets/b1/datasets/dataset_iter_1.csv.gz")
 
   # Etapas preprocesamiento
   CA_catastrophe_base( metodo="MachineLearning")
