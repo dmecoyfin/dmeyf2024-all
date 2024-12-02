c962bcd34d508da8db247ac6b9a832f8edcba5de
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-12-02 00:50:19 +0000

    auto commit 1       20241202 005012

--- src/workflows/z990_workflow_orden227_SEMI.r
+++ src/workflows/z990_workflow_orden227_SEMI.r
@@ -13,13 +13,13 @@ if( !exists("envg") ) envg <- env()  # global environment
 
 envg$EXPENV <- list()
 envg$EXPENV$bucket_dir <- "~/buckets/b1"
-envg$EXPENV$exp_dir <- "~/buckets/b1/expw_02/"
-envg$EXPENV$wf_dir <- "~/buckets/b1/flow_02/"
+envg$EXPENV$exp_dir <- "~/buckets/b1/expw227/"
+envg$EXPENV$wf_dir <- "~/buckets/b1/flow227/"
 envg$EXPENV$repo_dir <- "~/dmeyf2024/"
 envg$EXPENV$datasets_dir <- "~/buckets/b1/datasets/"
 envg$EXPENV$messenger <- "~/install/zulip_enviar.sh"
 
-envg$EXPENV$semilla_primigenia <- 881207
+envg$EXPENV$semilla_primigenia <- 102191
 
 # leo el unico parametro del script
 args <- commandArgs(trailingOnly=TRUE)
@@ -138,7 +138,7 @@ FEhist_base <- function( pinputexps)
 
   param_local$lag1 <- TRUE
   param_local$lag2 <- TRUE # no me engraso con los lags de orden 2
-  param_local$lag3 <- TRUE # no me engraso con los lags de orden 3
+  param_local$lag3 <- FALSE # no me engraso con los lags de orden 3
 
   # no me engraso las manos con las tendencias
   param_local$Tendencias1$run <- TRUE  # FALSE, no corre nada de lo que sigue
@@ -151,9 +151,9 @@ FEhist_base <- function( pinputexps)
   param_local$Tendencias1$ratiomax <- FALSE
 
   # no me engraso las manos con las tendencias de segundo orden
-  param_local$Tendencias2$run <- TRUE
+  param_local$Tendencias2$run <- FALSE
   param_local$Tendencias2$ventana <- 12
-  param_local$Tendencias2$tendencia <- TRUE
+  param_local$Tendencias2$tendencia <- FALSE
   param_local$Tendencias2$minimo <- FALSE
   param_local$Tendencias2$maximo <- FALSE
   param_local$Tendencias2$promedio <- FALSE
@@ -179,7 +179,7 @@ FErf_attributes_base <- function( pinputexps, ratio, desvio)
   # Parametros de un LightGBM que se genera para estimar la column importance
   param_local$train$clase01_valor1 <- c( "BAJA+2", "BAJA+1")
   param_local$train$training <- c( 202101, 202102, 202103)
-  
+
   # parametros para que LightGBM se comporte como Random Forest
   param_local$lgb_param <- list(
     # parametros que se pueden cambiar
@@ -267,12 +267,12 @@ TS_strategy_base8 <- function( pinputexps )
 
   param_local$final_train$undersampling <- 1.0
   param_local$final_train$clase_minoritaria <- c( "BAJA+1", "BAJA+2")
-  param_local$final_train$training <- c(202106, 202105, 202104, 202103, 202102, 202101)
+  param_local$final_train$training <- c(202106, 202105, 202104,
+    202103, 202102, 202101)
 
 
-
-  param_local$train$training <- c(202104, 202103, 202102, 202101, 202012, 202011)
-						   
+  param_local$train$training <- c(202104, 202103, 202102,
+    202101, 202012, 202011)
   param_local$train$validation <- c(202105)
   param_local$train$testing <- c(202106)
 
@@ -294,12 +294,12 @@ TS_strategy_base8 <- function( pinputexps )
 
   param_local$meta$script <- "/src/wf-etapas/z2101_TS_training_strategy.r"
 
-  param_local$future <- c(202109)
+  param_local$future <- c(202108)
 
   param_local$final_train$undersampling <- 0.02
   param_local$final_train$clase_minoritaria <- c( "BAJA+1", "BAJA+2")
   param_local$final_train$training <- c(
-    202107,202106, 202105, 202104, 202103, 202102, 202101, 
+    202106, 202105, 202104, 202103, 202102, 202101, 
     202012, 202011, 202010, 202009, 202008, 202007, 
     # 202006  Excluyo por variables rotas
     202005, 202004, 202003, 202002, 202001,
@@ -311,11 +311,11 @@ TS_strategy_base8 <- function( pinputexps )
   )
 
 
-  param_local$train$testing <- c(202107)
-  param_local$train$validation <- c(202106)
+  param_local$train$testing <- c(202106)
+  param_local$train$validation <- c(202105)
 
-   param_local$train$training <- c(
-    202105,202104, 202103, 202102, 202101, 
+  param_local$train$training <- c(
+    202104, 202103, 202102, 202101, 
     202012, 202011, 202010, 202009, 202008, 202007, 
     # 202006  Excluyo por variables rotas
     202005, 202004, 202003, 202002, 202001,
@@ -326,7 +326,7 @@ TS_strategy_base8 <- function( pinputexps )
     201904, 201903
   )
 
-																   						
+
   # Atencion  0.2  de  undersampling de la clase mayoritaria,  los CONTINUA
   # 1.0 significa NO undersampling
   param_local$train$undersampling <- 0.02
@@ -455,16 +455,16 @@ KA_evaluate_kaggle_semillerio <- function( pinputexps )
 {
   if( -1 == (param_local <- exp_init())$resultado ) return( 0 )# linea fija
 
-  param_local$meta$script <- "/src/wf-etapas/z2603_KA_evaluate_kaggle_SEMI.r"
+  param_local$meta$script <- "/src/wf-etapas/z2602_KA_evaluate_kaggle_SEMI.r"
 
   param_local$semilla <- NULL  # no usa semilla, es deterministico
 
   param_local$irepes_submit <- 1:20 # misterioso parametro, no preguntar
 
-  param_local$envios_desde <- 8000L
-  param_local$envios_hasta <- 14000L
+  param_local$envios_desde <- 10500L
+  param_local$envios_hasta <- 12050L
   param_local$envios_salto <-   500L
-  param_local$competition <- "dm-ey-f-2024-tercera"
+  param_local$competition <- "dm-ey-f-2024-segunda"
 
   return( exp_correr_script( param_local ) ) # linea fija
 }
@@ -476,12 +476,12 @@ KA_evaluate_kaggle_semillerio <- function( pinputexps )
 # Que predice 202107 donde conozco la clase
 # y ya genera graficos
 
-wf_SEMI_ago_orden_02 <- function( pnombrewf )
+wf_SEMI_ago_orden227 <- function( pnombrewf )
 {
   param_local <- exp_wf_init( pnombrewf ) # linea fija
 
   # Etapa especificacion dataset de la Segunda Competencia Kaggle
-  DT_incorporar_dataset( "~/buckets/b1/datasets/competencia_03.csv.gz")
+  DT_incorporar_dataset( "~/buckets/b1/datasets/competencia_02.csv.gz")
 
   CA_catastrophe_base( metodo="MachineLearning")
   FEintra_manual_base()
@@ -517,4 +517,6 @@ wf_SEMI_ago_orden_02 <- function( pnombrewf )
 # Aqui comienza el programa
 
 # llamo al workflow con future = 202108
-wf_SEMI_ago_orden_02()
+wf_SEMI_ago_orden227()
+
+
