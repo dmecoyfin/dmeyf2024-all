f4bd036d6a447880cddfe644b9a1fab98679dfd3
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-12-04 10:14:28 +0000

    auto commit 1       20241204 101419

--- src/workflows/909_run_orden227.r
+++ src/workflows/909_run_orden227.r
@@ -1,7 +1,7 @@
 require("rlang")
 
 # workflow que voy a correr
-PARAM <- "src/workflows/990_workflow_orden227_SEMI_rank_cero_fijo_sin202104_comp3_experimentacion1.r"
+PARAM <- "src/workflows/990_workflow_orden227_SEMI_rank_cero_fijo_sin202104_comp3_corregido6.r"
 
 envg <- env()
 
